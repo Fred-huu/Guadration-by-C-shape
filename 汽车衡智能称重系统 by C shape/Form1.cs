c5ebb0e2f7730841f2ead268963777fb44ef05d6
Fred-huu <hyd19931224@live.com>
2016-04-10 12:45:07 +0800

    添加项目文件。

---
+++ 汽车衡智能称重系统 by C shape/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 汽车衡智能称重系统_by_C_shape
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
